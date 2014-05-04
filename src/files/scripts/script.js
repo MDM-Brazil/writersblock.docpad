7b82902b13a20d3c4681f775073494a1873eeec4
Mikeumus <mike@betabulls.com>
2014-05-04 05:43:54 -0400

    TypeForm update.

--- src/files/scripts/script.js
+++ src/files/scripts/script.js
@@ -5,7 +5,4 @@ $( document ).ready(function() {
 		$('#homeContactModal').modal('show');
 	});
 	
-	$('.home-contact-modal').css('margin','-20px -20px 0');
-	//$('.modal-content')
-	// $('#typeform').css('margin','-166px 0 0 0');
 });
